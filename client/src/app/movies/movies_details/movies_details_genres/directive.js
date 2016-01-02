6051f764a88b46fae9601569cb14d7629f607e4c
lealivran <lealivran@msn.com>
2016-01-02 19:13:25 +0100

    add page listing genres de movie

---
+++ client/src/app/movies/movies_details/movies_details_genres/directive.js
@@ -0,0 +1,25 @@
+(function() {
+    'use strict';
+
+    function movieDetailsGenres() {
+        return {
+            restrict: 'EA', 
+            replace: true,
+            templateUrl: './src/app/movies/movies_details/movies_details_genres/template.html',
+            scope: {},
+            controllerAs: 'vm',
+            controller: function($log, MovieService, $stateParams) {
+                var vm = this;
+                MovieService.getOneMovie($stateParams.id)
+                    .then(function(movie) {
+                        vm.movie = movie;
+                    })
+
+            }            
+        };
+    }
+
+angular.module('movieDetailsGenresDirective', ['services.movie_details_genres'])
+    .directive('movieDetailsGenresDirective', movieDetailsGenres);
+
+})();
