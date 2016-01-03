812ffe3b8d57e97d4d954c58ca0b380e89b6da9b
lealivran <lealivran@msn.com>
2016-01-03 23:16:25 +0100

    conflicts fixed

--- client/src/app/movies/movies_details/route.js
+++ client/src/app/movies/movies_details/route.js
@@ -7,13 +7,7 @@
             url: '/movie/details/:id',
             views: {
                 '@': {
-<<<<<<< HEAD
-                    template: '<h1>Details</h1><movie-details-directive></movie-details-directive>',
-||||||| merged common ancestors
-                    template: '<h1>details</h1><movie-details-directive></movie-details-directive>',
-=======
                     template: '<movie-details-directive></movie-details-directive>',
->>>>>>> dev_lea
                 }
             }
         });
